222861133a1948ad7b294604af521e499b87faf8
Kristof Cserpes <kristofcserpes@gmail.com>
2019-05-14 12:36:28 +0200

    some cleaning up

--- src/LinearRegression.py
+++ src/LinearRegression.py
@@ -4,8 +4,7 @@ import numpy as np
 class LinearRegression:
 
     def __init__(self, data, valueToPredict, learningRate):
-        self.data = data
-        self.dataSize = float(len(self.data))
+        self.dataSize = float(len(data))
 
         self.learningRate = learningRate
         self.valueToPredict = valueToPredict
@@ -23,19 +22,17 @@ class LinearRegression:
     def total_error(self):
         sumError = 0
         for currentLine, y in zip(self.X, self.Y):
-            sumError += (self.predict(currentLine) - y)
+            sumError += 0.5 * (self.predict(currentLine) - y)
         return sumError / self.dataSize
 
     def step(self):
         new_grad_weight = np.zeros(len(self.weights))
         new_grad_bias = 0
-        j = 0
         for current_data, actual in zip(self.X, self.Y):
             tmp = self.predict(current_data) - actual
-            j += 1
             for i in range(len(current_data)):
-                new_grad_weight[i] += 0.5 * tmp * current_data[i]
-            new_grad_bias += 0.5 * tmp
+                new_grad_weight[i] += tmp * current_data[i]
+            new_grad_bias += tmp
         self.weights = np.subtract(self.weights,
                                    (new_grad_weight/self.dataSize) * self.learningRate)
         self.bias -= (new_grad_bias/self.dataSize) * self.learningRate
